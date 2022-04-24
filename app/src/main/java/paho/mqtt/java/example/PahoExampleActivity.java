017ab5acc7db7d3eb1f14f89616b521f09ee59d9
shcha <shcha@amtel.co.kr>
2022-04-24 13:49:52 +0900

    initial commit

---
+++ app/src/main/java/paho/mqtt/java/example/PahoExampleActivity.java
@@ -0,0 +1,268 @@
+/*******************************************************************************
+ * Copyright (c) 1999, 2016 IBM Corp.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * and Eclipse Distribution License v1.0 which accompany this distribution.
+ *
+ * The Eclipse Public License is available at
+ *    http://www.eclipse.org/legal/epl-v10.html
+ * and the Eclipse Distribution License is available at
+ *   http://www.eclipse.org/org/documents/edl-v10.php.
+ *
+ */
+package paho.mqtt.java.example;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import org.eclipse.paho.android.service.MqttAndroidClient;
+import org.eclipse.paho.client.mqttv3.DisconnectedBufferOptions;
+import org.eclipse.paho.client.mqttv3.IMqttActionListener;
+import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken;
+import org.eclipse.paho.client.mqttv3.IMqttMessageListener;
+import org.eclipse.paho.client.mqttv3.IMqttToken;
+import org.eclipse.paho.client.mqttv3.MqttCallbackExtended;
+import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
+import org.eclipse.paho.client.mqttv3.MqttException;
+import org.eclipse.paho.client.mqttv3.MqttMessage;
+
+import java.util.ArrayList;
+
+public class PahoExampleActivity extends AppCompatActivity {
+    private HistoryAdapter mAdapter;
+
+    MqttAndroidClient mqttAndroidClient;
+
+//    final String serverUri = "tcp://iot.eclipse.org:1883";
+    final String serverUri = "tcp://iot.amtel.co.kr";
+//    final String serverUri = "tcp://broker.hivemq.com:1883";
+
+//    String clientId = "ExampleAndroidClient";
+    String clientId = "Ams5GIOT";
+    final String subscriptionTopic = "v1/devices/me/telemetry";
+//    final String publishTopic = "exampleAndroidPublishTopic";
+    final String publishTopic = "v1/devices/me/telemetry";
+//    final String publishTopic = "/am/5g/test";
+//    final String publishMessage = "Hello World!";
+//    final String publishMessage = "{\"wifi_state\":\"1\"}";
+    final String publishMessage = "{" +
+        "\"is_cctv\":\"0\"," +
+        "\"deviceSoftwareVersion\":\"1.0\"," +
+        "\"line1Number\":\"010-1234-4567\"," +
+        "\"networkOperator\":\"45006\"," +
+        "\"networkOperatorName\":\"LG U+\"," +
+        "\"subscriberId\":\"0\"," +
+        "\"latitude\":\"37.4070040\"," +
+        "\"longitude\":\"127.1029780\"," +
+        "\"service_state\":\"0\"," +
+        "\"wifi_state\":\"0\"," +
+        "\"bt_state\":\"0\"," +
+        "\"batt_state\":\"80\"," +
+        "\"availableMemory\":\"50\"," +
+        "\"ramAvailablePercentage\":\"0\"," +
+        "\"rssi\":\"-1\"," +
+        "\"rsrp\":\"-1\"," +
+        "\"rsrq\":\"-1\"," +
+        "\"cpu_thermal\":\"40\"" +
+        "}";
+    MainHandler mMainHandler = new MainHandler();
+    final String KEY_RECEIVED_MESSAGE = "receivedMessage";
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_scrolling);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(view -> publishMessage());
+
+
+        RecyclerView mRecyclerView = (RecyclerView) findViewById(R.id.history_recycler_view);
+        RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+
+        mAdapter = new HistoryAdapter(new ArrayList<String>());
+        mRecyclerView.setAdapter(mAdapter);
+
+        clientId = clientId + System.currentTimeMillis();
+
+        mqttAndroidClient = new MqttAndroidClient(getApplicationContext(), serverUri, clientId);
+        mqttAndroidClient.setCallback(new MqttCallbackExtended() {
+            @Override
+            public void connectComplete(boolean reconnect, String serverURI) {
+
+                if (reconnect) {
+                    addToHistory("Reconnected to : " + serverURI);
+                    // Because Clean Session is true, we need to re-subscribe
+                    subscribeToTopic();
+                } else {
+                    addToHistory("Connected to: " + serverURI);
+                }
+            }
+
+            @Override
+            public void connectionLost(Throwable cause) {
+                addToHistory("The Connection was lost.");
+            }
+
+            @Override
+            public void messageArrived(String topic, MqttMessage message) throws Exception {
+                addToHistory("Incoming message: " + new String(message.getPayload()));
+            }
+
+            @Override
+            public void deliveryComplete(IMqttDeliveryToken token) {
+
+            }
+        });
+
+        MqttConnectOptions mqttConnectOptions = new MqttConnectOptions();
+        mqttConnectOptions.setAutomaticReconnect(true);
+        mqttConnectOptions.setCleanSession(false);
+        mqttConnectOptions.setUserName("gbsa_test_01");
+
+        try {
+            //addToHistory("Connecting to " + serverUri);
+            mqttAndroidClient.connect(mqttConnectOptions, null, new IMqttActionListener() {
+                @Override
+                public void onSuccess(IMqttToken asyncActionToken) {
+                    DisconnectedBufferOptions disconnectedBufferOptions = new DisconnectedBufferOptions();
+                    disconnectedBufferOptions.setBufferEnabled(true);
+                    disconnectedBufferOptions.setBufferSize(100);
+                    disconnectedBufferOptions.setPersistBuffer(false);
+                    disconnectedBufferOptions.setDeleteOldestMessages(false);
+                    mqttAndroidClient.setBufferOpts(disconnectedBufferOptions);
+                    subscribeToTopic();
+                }
+
+                @Override
+                public void onFailure(IMqttToken asyncActionToken, Throwable exception) {
+                    addToHistory("Failed to connect to: " + serverUri);
+                }
+            });
+
+
+        } catch (MqttException ex){
+            ex.printStackTrace();
+        }
+
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        try {
+            mqttAndroidClient.disconnect();
+        } catch (MqttException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void addToHistory(String mainText){
+        System.out.println("LOG: " + mainText);
+        mAdapter.add(mainText);
+        Snackbar.make(findViewById(android.R.id.content), mainText, Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show();
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void subscribeToTopic(){
+        try {
+            /*mqttAndroidClient.subscribe(subscriptionTopic, 0, null, new IMqttActionListener() {
+                @Override
+                public void onSuccess(IMqttToken asyncActionToken) {
+                    addToHistory("Subscribed!");
+                }
+
+                @Override
+                public void onFailure(IMqttToken asyncActionToken, Throwable exception) {
+                    addToHistory("Failed to subscribe");
+                }
+            });*/
+
+            // THIS DOES NOT WORK!
+            mqttAndroidClient.subscribe(subscriptionTopic, 0, new IMqttMessageListener() {
+                @Override
+                public void messageArrived(String topic, MqttMessage message) throws Exception {
+                    // to main thread
+//                    addToHistory("Incoming message: " + new String(message.getPayload()));
+                    Message msg = mMainHandler.obtainMessage();
+                    Bundle bundle = new Bundle();
+                    bundle.putString(KEY_RECEIVED_MESSAGE, new String(message.getPayload()));
+                    msg.setData(bundle);
+                    mMainHandler.sendMessage(msg);
+                    // message Arrived!
+                    System.out.println("Message: " + topic + " : " + new String(message.getPayload()));
+                }
+            });
+
+        } catch (MqttException ex){
+            System.err.println("Exception whilst subscribing");
+            ex.printStackTrace();
+        }
+    }
+
+    class MainHandler extends Handler {
+        @Override
+        public void handleMessage(@NonNull Message msg) {
+            super.handleMessage(msg);
+
+            Bundle bundle = msg.getData();
+            addToHistory(bundle.getString(KEY_RECEIVED_MESSAGE));
+        }
+    }
+
+
+    public void publishMessage(){
+
+        try {
+            MqttMessage message = new MqttMessage();
+            message.setPayload(publishMessage.getBytes());
+            mqttAndroidClient.publish(publishTopic, message);
+            addToHistory("Message Published");
+            if(!mqttAndroidClient.isConnected()){
+                addToHistory(mqttAndroidClient.getBufferedMessageCount() + " messages in buffer.");
+            }
+        } catch (MqttException e) {
+            System.err.println("Error Publishing: " + e.getMessage());
+            e.printStackTrace();
+        }
+    }
+
+}
